49065391674abfc1beb64f2298527db66c03d203
NIK78726 <ShkolnikovNikolai2012@yandex.ru>
2022-10-17 10:16:03 +0300

    17 10 2022

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -34,6 +34,8 @@ INSTALLED_APPS = [
     # Мои приложения
     'learning_logs',
     'users',
+    # Сторонние приложения
+    'bootstrap4',
     #Приложения Джанго по умолчанию
     'django.contrib.admin',
     'django.contrib.auth',
